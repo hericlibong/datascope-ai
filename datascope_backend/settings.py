c75e23717584ae31079bb9bfdcd9259727774f5c
hericlibong <heric.afrimages@gmail.com>
2025-05-22 11:35:31 +0200

    add specifics urls by app

--- datascope_backend/settings.py
+++ datascope_backend/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'analysis',
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +77,19 @@ WSGI_APPLICATION = 'datascope_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":     config("DB_NAME"),
+        "USER":     config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST":     config("DB_HOST", default="127.0.0.1"),
+        "PORT":     config("DB_PORT", default="5432"),
     }
 }
+
+
 
 
 # Password validation
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+
+
+urlpatterns = [
+    # routes à venir
+]
