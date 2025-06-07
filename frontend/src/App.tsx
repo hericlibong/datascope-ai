b12699b5c4a3f3f12e5134b1c67c1ee49461a44e
hericlibong <heric.afrimages@gmail.com>
2025-06-07 15:33:46 +0200

    Integration Shadcn UI & button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-800">
         Hello DataScope 👋
       </h1>
+      <Button>
+        Bouton Shadcn UI
+      </Button>
     </div>
   );
 }
